ba837ff5e4e9feb9416ab51b748128aee3c5be8a
Lyneisha Dickenson <70129828+NeishaD94@users.noreply.github.com>
2021-01-25 21:38:33 -0500

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,73 +1,97 @@
-let secretNumber = Math.trunc(Math.random() * 100) + 1;
-let score = document.getElementById('second').textContent;
-let highScore = document.getElementById('third').textContent;
+let avgNets = (96 + 108 + 89) / 3;
+let avgKnicks = (88 + 91 + 110) / 3;
 
-const displaymsg = function (message) {
-    document.getElementById('first').textContent = message;
+if (avgNets > avgKnicks) {
+    console.log('Nets wins the trophy');
+}
+else if (avgNets < avgKnicks) {
+    console.log('Knicks wins the trophy');
+}
+else {
+    console.log('There is a draw');
+}
+
+//BONUS 1: 
+
+avgNets = (97 + 112 + 101) / 3;
+avgKnicks = (109 + 95 + 123) / 3;
+
+if ((avgNets > avgKnicks) && (avgNets >= 100)) {
+    console.log('Nets wins the trophy');
+}
+
+else if ((avgNets < avgKnicks) && (avgKnicks >= 100)) {
+    console.log('Knicks wins the trophy');
+}
+
+else {
+    console.log("There is a draw");
+}
+
+//BONUS 2: 
+avgNets = (97 + 112 + 101) / 3;
+avgKnicks = (109 + 95 + 106) / 3;
+
+if ((avgNets > avgKnicks) && (avgNets >= 100)) {
+    console.log('Nets wins the trophy');
+}
+
+else if ((avgNets < avgKnicks) && (avgKnicks >= 100)) {
+    console.log('Knicks wins the trophy');
+}
+
+else if ((avgNets === avgKnicks) && (avgNets >= 100)) {
+    console.log("There is a draw");
+}
+
+//B
+
+let tip = 0;
+//Test data 1: 
+let testData = [275, 28, 430];
+let bill = 0;
+for (let i = 0; i < 3; i++) {
+    bill = testData[i];
+
+
+    let check = ((bill >= 30) && (bill <= 300));
+
+    switch (check) {
+        case true:
+            tip = ((15 * bill) / 100);
+            break;
+
+        case false:
+            tip = ((20 * bill) / 100);
+            break;
+        default:
+            console.log('Invalid value');
+            break;
+
+    }
+
+
+    let total = bill + tip;
+
+    console.log(`Test data ${i + 1}: The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
+
+}
+//C:
+
+
+function celsiusToFahrenheit(tempInC) {
+    let celsiusInF = ((9 / 5) * (tempInC) + 32);
+    console.log(`${tempInC} degrees C is ${celsiusInF} degrees F`);
+}
+
+function fahrenheitToCelsius(tempInF) {
+    let fahrenInC = ((tempInF - 32) * 5) / 9;
+
+    console.log(`${tempInF} degrees F is ${fahrenInC} degrees C`);
 }
 
 
-document.getElementById('btn2').addEventListener("click", function () {
-    const userInput = Number(document.querySelector('.text').value);
-
-    if (isNaN(userInput) || (userInput < 0) || (userInput > 100)) {
-        if (document.getElementById('first')) {
-            displaymsg('⛔ Invalid Input');
-
-        }
-    }
-    else if (!userInput) {
-        displaymsg('⛔ No number');
-    }
-
-    else if (userInput !== secretNumber && (score >= 1)) {
-        score = score - 1;
-        document.getElementById('second').textContent = score;
-        if (score === 0) {
-            if (document.getElementById('first')) {
-                displaymsg('⌛ Game Over');
-
-            }
-        }
-
-        else if (userInput > secretNumber) {
-            displaymsg('📈 Too High');
-
-        }
-        else if (userInput < secretNumber) {
-            displaymsg('📉 Too Low');
-
-        }
-    }
-    else if (userInput === secretNumber) {
-        displaymsg('🎉 Correct Number');
-        document.getElementById('qmark').textContent = secretNumber;
-        document.getElementById('qmark').style.backgroundColor = 'pink';
-        document.getElementById('third').textContent = score;
-        document.getElementById('bod').style.background = 'linear-gradient(to top right, green, lightblue)';
-
-    }
-
-
-
-
-
-
-
-});
-
-document.getElementById('btn1').addEventListener('click', function () {
-    document.getElementById('first').textContent = 'Start guessing...'
-    document.getElementById('second').textContent = '10';
-    document.getElementById('third').textContent = '0';
-    document.getElementById('qmark').textContent = '?';
-    document.querySelector('.text').textContent = " ";
-
-    secretNumber = Math.trunc(Math.random() * 100) + 1;
-    score = 10;
-
-    document.getElementById('qmark').style.backgroundColor = 'white';
-
-    document.getElementById('bod').style.background = 'linear-gradient(to top right, purple, lightyellow)';
-
-});
+let cels = 34;
+let fahren = 19;
+celsiusToFahrenheit(cels);
+fahrenheitToCelsius(fahren);
